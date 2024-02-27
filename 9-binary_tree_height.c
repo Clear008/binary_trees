dfe9d34feba427236c15c1c1a736c5b6d5126c66
Clear008 <wissal-exo@hotmail.com>
2024-02-27 15:59:12 +0100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,13 +11,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 size_t w_l = 0, w_r = 0;
 
 if (tree == NULL)
-
 {
-    return (0);
+return (0);
 }
 
 else
-
 {
 
 w_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
