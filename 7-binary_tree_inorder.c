56a04d6349c99c9b23751621c177c4aaa53a33d9
Clear008 <wissal-exo@hotmail.com>
2024-02-27 15:21:09 +0100

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -14,8 +14,8 @@ return;
 }
 else
 {
-binary_tree_preorder(tree->left, func);
+binary_tree_inorder(tree->left, func);
 func(tree->n);
-binary_tree_preorder(tree->right, func);
+binary_tree_inorder(tree->right, func);
 }
 }
