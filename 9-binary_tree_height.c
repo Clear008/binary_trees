c62afd2547892409be684fbcc59147c1f83838c0
Clear008 <wissal-exo@hotmail.com>
2024-02-27 15:35:32 +0100

    9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_height- function that that deletes an entire binary tree
+ * @tree: a pointer to the root node of the tree to be measured
+ * Return: the height of a binary tree
+ * 
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t w_l = 0, w_r = 0;
+
+if (tree == NULL)
+{
+    return (0);
+}
+else
+{
+w_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+w_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+}
+
+
+}
