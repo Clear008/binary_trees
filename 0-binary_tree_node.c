252b814191f44736febb884cceed3995e08853a8
Clear008 <wissal-exo@hotmail.com>
2024-02-27 13:12:31 +0100

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,13 +9,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *w_new;
+binary_tree_t *w_new;
 
 	w_new = malloc(sizeof(binary_tree_t));
 	if (w_new == NULL)
-    {
+{
 		return (NULL);
-    }
+}
 	w_new->n = value;
 	w_new->parent = parent;
 	w_new->left = NULL;
