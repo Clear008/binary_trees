149245dc853b07df484a87b4f317d3497c010275
Clear008 <wissal-exo@hotmail.com>
2024-02-28 17:13:47 +0100

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,24 +1,33 @@
 #include "binary_trees.h"
 
+#include"binary_trees.h"
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: A pointer to the root node of the tree to measure the height.
+ * binary_tree_height- function that that deletes an entire binary tree
+ * @tree: a pointer to the root node of the tree to be measured
+ * Return: the height of a binary tree
  *
- * Return: The height of the binary tree, or 0 if tree is NULL.
- */
+*/
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+size_t w_l = 0, w_r = 0;
 
 if (tree == NULL)
 {
 return (0);
 }
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
 
-return (1 + (left_height > right_height ? left_height : right_height));
+else
+{
+
+w_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+w_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+return ((w_l > w_r) ? w_l : w_r);
 }
+
+}
+
 
 /**
  * binary_tree_size-  function that measures the size of a BT
