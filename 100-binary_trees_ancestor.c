004aa569de289a2bac07c3cde4f710a7ce86b33d
Clear008 <wissal-exo@hotmail.com>
2024-02-29 15:46:16 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,27 +6,28 @@
  * @second: is a pointer to the second node
  * Return: a pointer to the lowest common ancestor node
 */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
- if (first == NULL || second == NULL)
-	{
-		return (NULL);
-	}
+if (first == NULL || second == NULL)
+{
+return (NULL);
+}
 
 if (first == second)
-	{
-		return ((binary_tree_t *)first);
-	}
+{
+return ((binary_tree_t *)first);
+}
 
 if (first->parent == NULL || first == second->parent ||
-    (!first->parent->parent && second->parent))
+(!first->parent->parent && second->parent))
 {
 return (binary_trees_ancestor(first, second->parent));
 }
-else if (second->parent == NULL || second == first->parent || 
+else if (second->parent == NULL || second == first->parent ||
 (!second->parent->parent && first->parent))
 {
 return (binary_trees_ancestor(first->parent, second));
 }
-return (binary_trees_ancestor(first->parent, second->parent));   
+return (binary_trees_ancestor(first->parent, second->parent));
 }
